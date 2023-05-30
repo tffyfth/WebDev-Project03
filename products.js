32d569e8be00c297cf4f8849b68dd7e0adac32d0
tffyfth <128219403+tffyfth@users.noreply.github.com>
2023-05-30 16:08:50 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Purse Wallet",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Simple coin purse with zipper.",
+    imgSrc: "img/p5.png",
+  },
+  {
+    id: 1,
+    name: "Purple Handbag",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Fluffy purple handbag.",
+    imgSrc: "img/p1.png",
+  },
+  {
+    id: 2,
+    name: "Cinderella Croptop",
+    price: 19.99,
+    instock: 10,
+    description:
+      "A cinderella inspired top.",
+    imgSrc: "img/p3.png",
+  },
+  {
+    id: 3,
+    name: "Coin Purse",
+    price: 25.99,
+    instock: 50,
+    description:
+      "Yarn knitted vintage style purse. ",
+    imgSrc: "img/p6.png",
+  },
+  {
+    id: 4,
+    name: "Cloudy Shoulder Bag",
+    price: 29.99,
+    instock: 40,
+    description:
+      "Inspired by the color palette of sky.",
+    imgSrc: "img/p2.png",
+  },
+  {
+    id: 5,
+    name: "Off-Shoulder Top",
+    price: 39.99,
+    instock: 40,
+    description:
+      "A cream cotton yarn-knitted top.",
+    imgSrc: "img/p4.png",
+  },
+];
